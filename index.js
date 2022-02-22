7b4717647ce3fbc0618a2092bee97814e00be348
Loleatha Martin <loleatha.joelle@gmail.com>
2022-02-22 01:34:51 -0500

    task 1 and 2 complete

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function myFunction() {
 myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
-
+/*Nested function and internal both lay within the function "myFunction." Anything within "myFunction" is considered to be inside of the lexical environment, which gives the nested function the go-to to reach out and grab that variable.*/
 
 
 
@@ -28,11 +27,19 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+function summation(number) {
+  let totalNumber = 0;
 
+  function counter() {
+    for (let i = 0; i <= number; i++) {
+      totalNumber += i;
+    }
+  }
+counter();
+return totalNumber;
   }
  
+console.log(summation(8));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
